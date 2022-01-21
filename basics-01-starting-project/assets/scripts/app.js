41321932c559500c32ada2b1ca7b8d30cf5ac0aa
isabellerod <isabeller29@hotmail.com>
2022-01-21 12:29:14 -0300

    function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
+let calculationDescription
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('the result is: ' + result);
+}
+
+add();
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-let calculationDescription = `(${defaultResult} +10) * 3 / 2 - 1`;
+calculationDescription = `(${defaultResult} +10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
