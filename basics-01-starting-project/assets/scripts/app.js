a00382c41905fab606ba3b0e349efc7b44e988be
isabellerod <isabeller29@hotmail.com>
2022-01-24 16:22:26 -0300

    splitting code

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUser(){
+    return +userInput;
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUser();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
-
-outputResult(currentResult, calculationDescription);
