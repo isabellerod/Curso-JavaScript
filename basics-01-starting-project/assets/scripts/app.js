22dd4ae788353dc305ffe5eb98aea916f57ae041
isabellerod <isabeller29@hotmail.com>
2022-01-25 10:17:11 -0300

    operators

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,40 +1,42 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from input field
 function getUser(){
     return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createOutput(operator, resultBefore, calcNumber) {
     const calcDescription = `${resultBefore} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
-}
+} // From vendor file
 
 function add() {
     const enteredNumber = getUser();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createOutput('+', initialResult, enteredNumber);
 }
 
 function subtract() {
     const enteredNumber = getUser();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createOutput('-', initialResult, enteredNumber);
 }
 
 function multiply() {
     const enteredNumber = getUser();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createOutput('*', initialResult, enteredNumber);
 }
 
 function divide () {
     const enteredNumber = getUser();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createOutput('/', initialResult, enteredNumber);
 }
 
