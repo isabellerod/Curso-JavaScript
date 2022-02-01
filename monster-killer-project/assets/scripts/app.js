0fc7b73f26efd95543adff3e9b9ffa730c0b76ac
isabellerod <isabeller29@hotmail.com>
2022-02-01 10:43:49 -0300

    win condition

--- monster-killer-project/assets/scripts/app.js
+++ monster-killer-project/assets/scripts/app.js
@@ -1,13 +1,25 @@
 const attackValue = 10;
+const monsterAttack = 14;
 let maxLife = 100;
 let monsterHealth = maxLife;
-let playerHealt = maxLife;
+let playerHealth = maxLife;
 
 adjustHealthBars(maxLife);
 
 function attack() {
     const damage = dealMonsterDamage(attackValue);
     monsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(monsterAttack);
+    playerHealth -= playerDamage;
+    if (monsterHealth <= 0 && playerHealth > 0) {
+        alert('You won!');
+    }
+    else if (playerHealth <= 0 && monsterHealth > 0) {
+        alert ('You lost!');
+    }
+    else if (playerHealth <= 0 && monsterHealth <= 0) {
+        alert('You have a draw!');
+    }
 }
 
 attackBtn.addEventListener('click', attack);
