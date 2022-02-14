915014d90c0cffe3c5349ec7d5cc5abdcfe93c8e
isabellerod <isabeller29@hotmail.com>
2022-02-14 15:45:16 -0300

    var vs let vs const

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,17 @@
+var nome = 'Isabelle';
+let hobbies;
+
+if (nome === 'Isabelle') {
+    hobbies = ['Sports', 'Cooking'];
+    console.log(hobbies);
+}
+
+function greet() {
+    var age = 30;
+    var nome = 'Manuel';
+    console.log(nome, age, hobbies);
+}
+
+console.log(nome, hobbies);
+
+greet();
