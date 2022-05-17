d9f2815e3b1c81191201dd248fec37158f65b259
isabellerod <isabeller29@hotmail.com>
2022-05-17 16:36:29 -0300

    adding user choices

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,5 +1,27 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-startGameBtn.addEventListener('click', function(){
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_CHOICE = ROCK;
+
+let gameRunning = false;
+
+const playerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert('Invalid choice! We chose Rock for you!')
+        return DEFAULT_CHOICE;
+    }
+    return selection;
+};
+
+startGameBtn.addEventListener('click', function() {
+    if (gameRunning) {
+        return;
+    }
+    gameRunning = true;
     console.log('Game is starting...');
+    const playerSelection = playerChoice();
+    console.log(playerSelection);
 });
