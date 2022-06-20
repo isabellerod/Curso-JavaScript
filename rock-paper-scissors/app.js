77de3f00bfdfb4c5ddfab8698eaeb450d4658ea8
isabellerod <isabeller29@hotmail.com>
2022-06-20 09:25:58 -0300

    Rest parameters (rest operator)

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -69,3 +69,25 @@ startGameBtn.addEventListener('click', () => {
     alert(message);
     gameRunning = false;
 });
+
+//not related to the game
+
+const sumUp = (a, b, ...numbers) => {
+    let sum = 0;
+    for (const num of numbers) {
+        sum += num;
+    }
+    return sum;
+};
+
+const subtractUp = function() {
+    let sum = 0;
+    for (const num of arguments) {
+        sum -= num;
+    }
+    return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20))
